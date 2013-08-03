3c7b42061e8025a600ae73dd467fa3510ee2e91a
crazysam <sambatista1@gmail.com>
2013-08-02 21:57:54 -0400

    Added basic player controller.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+	public int moveSpeed = 100;
+	public int rotateSpeed = 10;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+	{
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		CharacterController controller = GetComponent("CharacterController") as CharacterController;
+
+		int speed = moveSpeed;
+		if (Debug.isDebugBuild && Input.GetKey(KeyCode.LeftShift))
+			speed *= 2;
+
+		// Move forward / backward
+		if(Input.GetKey(KeyCode.W))
+			controller.SimpleMove(forward * (speed * Time.deltaTime));
+		else if(Input.GetKey(KeyCode.S))
+			controller.SimpleMove(-forward * (speed * Time.deltaTime));
+
+		// Rotate around y - axis
+		if (Input.GetKey(KeyCode.A))
+			transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+		else if (Input.GetKey(KeyCode.D))
+			transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
 	}
 }
