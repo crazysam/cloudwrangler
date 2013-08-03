aa4df69cb2799fe4861a1b8988be2a9ad49aa14a
U-Akragas\agrub_000 <agrub_000@Akragas.(none)>
2013-08-02 22:57:34 -0400

    Added cloud related stuffz

---
+++ Assets/Scripts/CloudController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * .05f * Time.deltaTime);
+        transform.Translate(Vector3.forward * .05f * Time.deltaTime);
+    }
+}
